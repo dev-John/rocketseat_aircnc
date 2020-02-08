3aeabfc121e5eefb642a70482f4ae4822d94da94
Jonatas Oliveira <jonatasoliveira1234@gmail.com>
2020-02-07 22:38:57 -0300

    Desenvolvimento da criação de reservas, mobile quase finalizado

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, Image, StyleSheet, Platform, AsyncStorage } from 'react-native';
+import { SafeAreaView, ScrollView, Image, StyleSheet, Platform, AsyncStorage } from 'react-native';
 
 import SpotList from '../components/SpotList';
 
@@ -19,7 +19,9 @@ export default function List() {
 	return (
 		<SafeAreaView style={styles.container}>
 			<Image style={styles.logo} source={logo}/>
-			<SpotList />
+			<ScrollView>
+				{techs.map(tech => <SpotList key={tech} tech={tech} />)}
+			</ScrollView>
 		</SafeAreaView>
 	)
 }
