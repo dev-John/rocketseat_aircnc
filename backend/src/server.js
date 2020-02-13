0ea262a1eca9c2131e81eb4f168066cbf5d25e41
Jonatas Oliveira <jonatasoliveira1234@gmail.com>
2020-02-12 22:32:14 -0300

    Finalizados o app, o web e o backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,16 +2,34 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
+const socketio = require('socket.io');
+const http = require('http');
 
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect('mongodb://mongoadmin:mongoadmin@cluster0-shard-00-00-8yykh.mongodb.net:27017,cluster0-shard-00-01-8yykh.mongodb.net:27017,cluster0-shard-00-02-8yykh.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
+const connectedUsers = { };
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+})
 
 //GET, POST, PUT, DELETE
 
@@ -24,4 +42,4 @@ app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, ScrollView, Image, StyleSheet, Platform, AsyncStorage } from 'react-native';
+import socketio from 'socket.io-client';
+import { SafeAreaView, ScrollView, Image, StyleSheet, Platform, AsyncStorage, Alert } from 'react-native';
 
 import SpotList from '../components/SpotList';
 
@@ -7,6 +8,18 @@ import logo from '../assets/logo.png';
 
 export default function List() {
 	const [techs, setTechs] = useState([]);
+
+	useEffect(() => {
+		AsyncStorage.getItem('user').then(user_id => {
+			const socket = socketio('http://192.168.1.36:3333', {
+				query: { user_id }
+			})
+
+			socket.on('booking_response', booking => {
+				Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REJEITADA'}`)
+			})
+		})
+	}, []);
 
 	useEffect(() => {
 		AsyncStorage.getItem('techs').then(storageTechs => {
